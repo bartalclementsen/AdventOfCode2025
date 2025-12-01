c816067e1d8a8eefe4601ecf23523d3119b8fd53
Bartal Clementsen <bac@formula.fo>
2025-12-01 08:52:28 +0000

    Day 01

---
+++ src/AdventOfCode2025.Client/Days/Day01/Solver.cs
@@ -0,0 +1,70 @@
+﻿
+namespace AdventOfCode2025.Client.Days.Day01;
+
+public class Solver : SolverBase
+{
+    public override string Solve1(IEnumerable<string> inputs)
+    {
+        int timesZeroHit = 0;
+        int currentNumber = 50;
+        foreach (string input in inputs)
+        {
+            string direction = input[..1];
+            int distance = int.Parse(input[1..]);
+
+            if (direction == "L")
+            {
+                currentNumber -= distance;
+            }
+            else if (direction == "R")
+            {
+                currentNumber += distance;
+            }
+
+            currentNumber = Mod(currentNumber, 100);
+
+            if (currentNumber == 0)
+            {
+                timesZeroHit++;
+            }
+        }
+
+        return timesZeroHit.ToString();
+    }
+
+    public override string Solve2(IEnumerable<string> inputs)
+    {
+        int timesZeroHit = 0;
+        int currentNumber = 50;
+        foreach (string input in inputs)
+        {
+            string direction = input[..1];
+            int distance = int.Parse(input[1..]);
+
+            for (int i = 0; i < distance; i++)
+            {
+                if (direction == "R")
+                {
+                    currentNumber++;
+                }
+                else
+                {
+                    currentNumber--;
+                }
+                currentNumber = Mod(currentNumber, 100);
+                if (currentNumber == 0)
+                {
+                    timesZeroHit++;
+                }
+            }
+        }
+
+        return timesZeroHit.ToString();
+    }
+
+    private int Mod(int x, int m)
+    {
+        int r = x % m;
+        return r < 0 ? r + m : r;
+    }
+}
---
+++ src/AdventOfCode2025.Tests/Days/Day01/SolverTests.cs
@@ -0,0 +1,89 @@
+﻿namespace AdventOfCode2025.Client.Days.Day01;
+
+public class SolverTests
+{
+    public Solver GetUnit()
+    {
+        return new();
+    }
+
+    [Fact]
+    public void Part1_Test_1()
+    {
+        // Arrange
+        IEnumerable<string> input = """
+            L68
+            L30
+            R48
+            L5
+            R60
+            L55
+            L1
+            L99
+            R14
+            L82
+            """.Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("3", result);
+    }
+
+    [Fact]
+    public void Part2_Test_1()
+    {
+        // Arrange
+        IEnumerable<string> input = """
+            L68
+            L30
+            R48
+            L5
+            R60
+            L55
+            L1
+            L99
+            R14
+            L82
+            """.Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("6", result);
+    }
+
+    [Fact]
+    public async Task Part1()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("1066", result);
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("6223", result);
+    }
+}
