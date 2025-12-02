b1355c5f49b03b8deaeaa6dd5670e881ab9668c2
Bartal Clementsen <bac@formula.fo>
2025-12-02 15:45:01 +0000

    Day02

---
+++ src/AdventOfCode2025.Client/Days/Day02/Solver.cs
@@ -0,0 +1,117 @@
+﻿
+namespace AdventOfCode2025.Client.Days.Day02;
+
+public class Solver : SolverBase
+{
+
+    public override string Solve1(IEnumerable<string> inputs)
+    {
+        //Parse
+        List<Range> ranges = [];
+        foreach (string input in inputs)
+        {
+            foreach (string range in input.Split(","))
+            {
+                string[] values = range.Split("-");
+                ranges.Add(new Range(long.Parse(values[0]), long.Parse(values[1])));
+            }
+        }
+
+        // Lookup Invalid Ids
+        long sum = 0;
+        foreach (Range range in ranges)
+        {
+            foreach (long id in range.GetInvalidId())
+            {
+                sum += id;
+            }
+        }
+
+        return sum.ToString();
+    }
+
+    public override string Solve2(IEnumerable<string> inputs)
+    {
+        //Parse
+        List<Range> ranges = [];
+        foreach (string input in inputs)
+        {
+            foreach (string range in input.Split(","))
+            {
+                string[] values = range.Split("-");
+                ranges.Add(new Range(long.Parse(values[0]), long.Parse(values[1])));
+            }
+        }
+
+        // Lookup Invalid Ids
+        long sum = 0;
+        foreach (Range range in ranges)
+        {
+            foreach (long id in range.GetMoreInvalidId())
+            {
+                sum += id;
+            }
+        }
+
+        return sum.ToString();
+    }
+
+    public record Range(long FirstId, long LastId)
+    {
+        public List<long> GetInvalidId()
+        {
+            List<long> invalidIds = [];
+            for (long i = FirstId; i <= LastId; i++)
+            {
+                string id = i.ToString();
+                if (id.Length % 2 != 0)
+                {
+                    continue;
+                }
+
+                string firstPart = id[..(id.Length / 2)];
+                string secondPart = id[(id.Length / 2)..];
+                if (firstPart == secondPart)
+                {
+                    invalidIds.Add(i);
+                }
+            }
+
+            return invalidIds;
+        }
+
+        public List<long> GetMoreInvalidId()
+        {
+            List<long> invalidIds = [];
+            for (long i = FirstId; i <= LastId; i++)
+            {
+                string id = i.ToString();
+                for (int j = 1; j < (id.Length / 2) + 1; j++)
+                {
+                    // Build a substring to check
+                    string toCheck = id[..j];
+                    int length = toCheck.Length;
+                    bool foundCounter = false;
+                    // Check if any substring matches the ToCheck variables
+                    for (int k = j; k < id.Length; k += length)
+                    {
+                        string checkAgainst = id.Substring(k, Math.Min(id.Length - k, length));
+                        if (toCheck != checkAgainst)
+                        {
+                            foundCounter = true;
+                            break;
+                        }
+                    }
+
+                    if (!foundCounter)
+                    {
+                        invalidIds.Add(i);
+                        break;
+                    }
+                }
+            }
+
+            return invalidIds;
+        }
+    }
+}
---
+++ src/AdventOfCode2025.Tests/Days/Day02/SolverTests.cs
@@ -0,0 +1,116 @@
+﻿namespace AdventOfCode2025.Client.Days.Day02;
+
+public class SolverTests
+{
+    public Solver GetUnit()
+    {
+        return new();
+    }
+
+    [Theory]
+    [InlineData("11-22", "2")]
+    [InlineData("95-115", "1")]
+    [InlineData("998-1012", "1")]
+    [InlineData("1188511880-1188511890", "1")]
+    [InlineData("222220-222224", "1")]
+    [InlineData("1698522-1698528", "0")]
+    [InlineData("446443-446449", "1")]
+    [InlineData("38593856-38593862", "1")]
+    public void Part1_Test_1(string ids, string invalidIds)
+    {
+        // Arrange
+        IEnumerable<string> input = ids.Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        var split = ids.Split("-");
+        var range = new Solver.Range(long.Parse(split[0]), long.Parse(split[1]));
+
+        // Assert
+        Assert.Equal(invalidIds, range.GetInvalidId().Count.ToString());
+    }
+
+    [Fact]
+    public void Part1_Test_2()
+    {
+        // Arrange
+        IEnumerable<string> input = "11-22, 95-115, 998-1012, 1188511880-1188511890, 222220-222224, 1698522-1698528, 446443-446449, 38593856-38593862".Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("1227775554", result);
+    }
+
+    [Theory]
+    [InlineData("11-22", "2")]
+    [InlineData("95-115", "2")]
+    [InlineData("998-1012", "2")]
+    [InlineData("1188511880-1188511890", "1")]
+    [InlineData("222220-222224", "1")]
+    [InlineData("1698522-1698528", "0")]
+    [InlineData("446443-446449", "1")]
+    [InlineData("38593856-38593862", "1")]
+    [InlineData("565653-565659", "1")]
+    [InlineData("824824821-824824827", "1")]
+    [InlineData("2121212118-2121212124", "1")]
+    public void Part2_Test_1(string ids, string invalidIds)
+    {
+        // Arrange
+        IEnumerable<string> input = ids.Split(Environment.NewLine);
+
+        // Act
+        var split = ids.Split("-");
+        var range = new Solver.Range(long.Parse(split[0]), long.Parse(split[1]));
+
+        // Assert
+        Assert.Equal(invalidIds, range.GetMoreInvalidId().Count.ToString());
+    }
+
+    [Fact]
+    public void Part2_Test_2()
+    {
+        // Arrange
+        IEnumerable<string> input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124".Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("4174379265", result);
+    }
+
+    [Fact]
+    public async Task Part1()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("21898734247", result);
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("28915664389", result);
+    }
+}
