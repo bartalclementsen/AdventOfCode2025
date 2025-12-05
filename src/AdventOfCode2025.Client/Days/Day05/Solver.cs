8fb2041a4d3b4d410021616aefc1698e51e17e9e
Bartal Clementsen <bac@formula.fo>
2025-12-05 09:04:44 +0000

    Day 05

---
+++ src/AdventOfCode2025.Client/Days/Day05/Solver.cs
@@ -0,0 +1,129 @@
+﻿namespace AdventOfCode2025.Client.Days.Day05;
+
+public class Solver : SolverBase
+{
+    public override string Solve1(IEnumerable<string> inputs)
+    {
+        List<FreshIngredientIdRange> ranges = [];
+        List<AvailableIngredientId> availableIngredients = [];
+
+        bool isRange = true;
+        foreach (string input in inputs)
+        {
+            if (input.Length == 0)
+            {
+                isRange = false;
+                continue;
+            }
+
+            if (isRange)
+            {
+                ranges.Add(new FreshIngredientIdRange
+                {
+                    IdFrom = long.Parse(input.Split('-')[0]),
+                    IdTo = long.Parse(input.Split('-')[1])
+                });
+            }
+            else
+            {
+                availableIngredients.Add(new AvailableIngredientId
+                {
+                    Id = long.Parse(input)
+                });
+            }
+        }
+
+        long freshCount = 0;
+        foreach (var availableIngredient in availableIngredients)
+        {
+            if (ranges.Any(r => r.Contains(availableIngredient.Id)))
+            {
+                freshCount++;
+            }
+        }
+
+
+        return freshCount.ToString();
+    }
+
+    public override string Solve2(IEnumerable<string> inputs)
+    {
+        List<FreshIngredientIdRange> ranges = [];
+        List<AvailableIngredientId> availableIngredients = [];
+
+        bool isRange = true;
+        foreach (string input in inputs)
+        {
+            if (input.Length == 0)
+            {
+                isRange = false;
+                continue;
+            }
+
+            if (isRange)
+            {
+                ranges.Add(new FreshIngredientIdRange
+                {
+                    IdFrom = long.Parse(input.Split('-')[0]),
+                    IdTo = long.Parse(input.Split('-')[1])
+                });
+            }
+            else
+            {
+                availableIngredients.Add(new AvailableIngredientId
+                {
+                    Id = long.Parse(input)
+                });
+            }
+        }
+
+        var sortedRanges = ranges.OrderBy(o => o.IdFrom).ThenBy(o => o.IdTo).ToList();
+
+        // Rebalance ranges
+        for (int i = 0; i < sortedRanges.Count; i++)
+        {
+            var range = sortedRanges[i];
+            for (int j = i + 1; j < sortedRanges.Count; j++)
+            {
+                var otherRange = sortedRanges[j];
+
+                if (otherRange.IsInside(range))
+                {
+                    sortedRanges.RemoveAt(j);
+                    j--;
+                }
+                else if (otherRange.IdFrom <= range.IdTo)
+                {
+                    otherRange.IdFrom = range.IdTo + 1;
+                }
+
+            }
+        }
+
+        return sortedRanges.Sum(r => r.Length).ToString();
+    }
+
+    public record AvailableIngredientId
+    {
+        public required long Id { get; init; }
+    }
+
+    public class FreshIngredientIdRange
+    {
+        public long IdFrom { get; set; }
+
+        public long IdTo { get; set; }
+
+        public long Length => IdTo - IdFrom + 1;
+
+        public bool Contains(long id)
+        {
+            return id >= IdFrom && id <= IdTo;
+        }
+
+        public bool IsInside(FreshIngredientIdRange other)
+        {
+            return IdFrom >= other.IdFrom && IdTo <= other.IdTo;
+        }
+    }
+}
---
+++ src/AdventOfCode2025.Tests/Days/Day05/SolverTests.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode2025.Client.Days.Day05;
+
+public class SolverTests
+{
+    public Solver GetUnit()
+    {
+        return new();
+    }
+
+    [Fact]
+    public void Part1_Test_1()
+    {
+        // Arrange
+        IEnumerable<string> input = """
+            3-5
+            10-14
+            16-20
+            12-18
+
+            1
+            5
+            8
+            11
+            17
+            32
+            """.Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("3", result);
+    }
+
+    [Fact]
+    public void Part2_Test_1()
+    {
+        // Arrange
+        IEnumerable<string> input = """
+            3-5
+            10-14
+            16-20
+            12-18
+            """.Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("14", result);
+    }
+
+    [Fact]
+    public async Task Part1()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("874", result);
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("348548952146313", result);
+    }
+}
