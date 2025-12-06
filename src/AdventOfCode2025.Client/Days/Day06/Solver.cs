0946dd90a512b461a34f555a6eb0acecc715b67c
Bartal Clementsen <bartalclementsen@hotmail.com>
2025-12-06 15:56:00 +0000

    Day06

---
+++ src/AdventOfCode2025.Client/Days/Day06/Solver.cs
@@ -0,0 +1,157 @@
+﻿
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AdventOfCode2025.Client.Days.Day06;
+
+public class Solver : SolverBase
+{
+    public override string Solve1(IEnumerable<string> inputs)
+    { 
+
+        List<Problem> problems = [];
+        foreach (string input in inputs)
+        {
+            string[] elements = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < elements.Length; i++)
+            {
+                string element = elements[i];
+                Problem problem = new();
+                if (problems.Count <= i)
+                {
+                    problems.Add(problem);
+                }
+                else
+                {
+                    problem = problems[i];
+                }
+
+
+                if (long.TryParse(element, out long number))
+                {
+                    problem.AddNumber(number);
+                }
+                else
+                {
+                    problem.AddOperation(element switch
+                    {
+                        "+" => Operation.Add,
+                        "*" => Operation.Multiply,
+                        _ => throw new InvalidOperationException("Unknown operation")
+                    });
+                }
+
+
+            }
+
+        }
+
+        return problems.Sum(o => o.Solve()).ToString();
+    }
+
+    public override string Solve2(IEnumerable<string> inputs)
+    {
+        List<Problem> problems = [];
+
+        StringBuilder sb = new();
+        int height = inputs.Count();
+        int width = inputs.First().Length;
+        char[,] array = new char[height, width];
+        Problem problem = new();
+        problems.Add(problem);
+        
+        for (int i = width - 1; i >= 0; i--)
+        {
+            bool emptyLine = true;
+            string number = "";
+
+            for (int j = 0; j < height; j++)
+            {
+                if(j >= inputs.Count())
+                {
+                    continue;
+                }
+
+                string input = inputs.ElementAt(j);
+                if (i >= input.Length)
+                {
+                    continue;
+                }
+                var element = input[i];
+                //array[i, j] = element;
+
+                if (element == ' ')
+                {
+                    // skip
+                    continue;
+                }
+                else if (char.IsDigit(element))
+                {
+                    emptyLine = false;
+                    number = number + element.ToString();
+                }
+                else
+                {
+                    emptyLine = false;
+                    problem.AddOperation(element switch
+                    {
+                        '+' => Operation.Add,
+                        '*' => Operation.Multiply,
+                        _ => throw new InvalidOperationException("Unknown operation")
+                    });
+                }
+            }
+
+            if(!emptyLine)
+            {
+                problem.AddNumber(long.Parse(number));
+            }
+            else
+            {
+                problem = new Problem();
+                problems.Add(problem);
+            }
+
+            sb.AppendLine();
+        }
+
+        var s = sb.ToString();
+
+
+        return problems.Sum(o => o.Solve()).ToString();
+    }
+
+
+    public class Problem
+    {
+        private readonly List<long> _number = [];
+
+        private Operation _operation;
+
+        public void AddNumber(long number)
+        {
+            _number.Add(number);
+        }
+
+        public void AddOperation(Operation operation)
+        {
+            _operation = operation;
+        }
+
+        public long Solve()
+        {
+            return _operation switch
+            {
+                Operation.Add => _number.Sum(),
+                Operation.Multiply => _number.Aggregate(1L, (a, b) => a * b),
+                _ => throw new InvalidOperationException("Unknown operation")
+            };
+        }
+    }
+
+    public enum Operation
+    {
+        Add,
+        Multiply
+    }
+}
---
+++ src/AdventOfCode2025.Tests/Days/Day06/SolverTests.cs
@@ -0,0 +1,77 @@
+﻿namespace AdventOfCode2025.Client.Days.Day06;
+
+public class SolverTests
+{
+    public Solver GetUnit()
+    {
+        return new();
+    }
+
+    [Fact]
+    public void Part1_Test_1()
+    {
+        // Arrange
+        IEnumerable<string> input = """
+            123 328  51 64 
+             45 64  387 23 
+              6 98  215 314
+            *   +   *   + 
+            """.Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("4277556", result);
+    }
+
+    [Fact]
+    public void Part2_Test_1()
+    {
+        // Arrange
+        IEnumerable<string> input = """
+            123 328  51 64 
+             45 64  387 23 
+              6 98  215 314
+            *   +   *   + 
+            """.Split(Environment.NewLine);
+
+        Solver unit = GetUnit();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("3263827", result);
+    }
+
+    [Fact]
+    public async Task Part1()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve1(input);
+
+        // Assert
+        Assert.Equal("5977759036837", result);
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        // Arrange
+        Solver unit = GetUnit();
+        string[] input = await unit.GetInputsAsync();
+
+        // Act
+        string result = unit.Solve2(input);
+
+        // Assert
+        Assert.Equal("9630000828442", result);
+    }
+}
